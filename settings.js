198a507a6e2f70f246f07fe21746fa0388e48cb8
botmke0 <112796316+botmke0@users.noreply.github.com>
2024-08-20 03:21:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,18 +10,18 @@ global.location = "Pakistan, Sialkot, Lahore" //ur location
 
 //new
 global.botname = '𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur owner name
+global.ownernumber = ['919074597113'] //ur owner number, dont add more than one
+global.ownername = '𔗨🍷𝑖 𝑤𝑎𝑛𝑛𝑎 𔘓𓂃' //ur owner name
 global.websitex = "https://youtube.com/GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
 global.wm = "Global Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD15' //script link
-global.packname = "Sticker By"
-global.author = "𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃\n\n+923444844060"
+global.packname = ""𝆕ʟᴜᴠ𓆩♡𓆪᭕.ᴄᴏᴍ"
+global.author = "𝐌𝐞𝐨𝐰𝐰🧸💗`  𝆕\n\n+919074597113"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["919074597113"] // Premium User
 
 //channel id
 global.xchannel = {
